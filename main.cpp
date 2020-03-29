19b946cc267509a90a58be719876c6b57060707b
frieder-keilholz <fgk1999@outlook.de>
2020-03-29 13:43:24 +0200

    improved naming

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@ using namespace std;
 int main()
 {
   cout << "Hello World!" << endl << "Classe bauen:";
-  Node *node = new Node("Ich bin eine Klasse!");
+  Node *node = new Node("Node 1");
 
   cout << node->getName() << endl;
   
-  node->setName("Und ich heiße jetzt anders :]");
+  node->setName("Node 1 :)");
   
   cout << node->getName() << endl;
 
-  Node *child1 = new Node("Im child 1");
-  Node *child2 = new Node("Im child 2");
+  Node *child1 = new Node("Node 2");
+  Node *child2 = new Node("Node 3");
   child1->addChild(child2);
   node->addChild(child1);
 
